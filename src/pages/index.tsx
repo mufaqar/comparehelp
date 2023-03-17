9254a2a9b7ace29224846fb70ee99cf5f1af9bf4
Mufaqar <mufaqar@gmail.comm>
2023-03-17 23:08:03 +0500

    New changes

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,7 +6,7 @@ export default function Home() {
     <>
       <Head>
         <title>CompareHelp | Collection of Personal Data Statement</title>
-        <meta name="description" content="Generate several questions in a single click." />
+        <meta name="description" content="Generate several questions" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
